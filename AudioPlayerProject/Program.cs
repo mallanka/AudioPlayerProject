a7bed314574110ed23c58ae7a63d838436448d0c
Alexander Naumkin <mallanka2byrzoomkin@yandex.ru>
2019-05-15 21:19:26 +0300

    AL4-Player1/1. IDisposable

--- AudioPlayerProject/Program.cs
+++ AudioPlayerProject/Program.cs
@@ -10,14 +10,11 @@ namespace AudioPlayerProject
     {
         static void Main(string[] args)
         {
-            Player player = new Player();
-            player.LoadSongs("f://m//");
-            player.List();
-            player.SaveAsPlaylist("f://playlist.xml");
-            player.ClearList();
-            Console.WriteLine();
-            player.LoadPlaylist("f://playlist.xml");
-            player.List();
+            using (Player player = new Player())
+            {
+                player.LoadSongs("f://m//");
+                player.Play();
+            }
         }
     }
 }
