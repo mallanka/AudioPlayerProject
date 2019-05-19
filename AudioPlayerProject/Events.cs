c114d97f0307f159b2f2dafde75763016f1b19e6
Alexander Naumkin <mallanka2byrzoomkin@yandex.ru>
2019-05-19 19:03:29 +0300

    A.L7.Player1/2. Events

---
+++ AudioPlayerProject/Events.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AudioPlayerProject
+{
+    class PlayerStartEventClass
+    {
+        public void Message()
+        {
+            Console.WriteLine("Player is started");
+        }
+    }
+    class PlayerStopEventClass
+    {
+        public void Message()
+        {
+            Console.WriteLine("Player is stopped");
+        }
+    }
+    class SongrPlayEventClass
+    {
+        public void Message()
+        {
+            Console.WriteLine("Song is playing");
+        }
+    }
+    class SongListChangedEventClass
+    {
+        public void Message()
+        {
+            Console.WriteLine("Playlist was changed");
+        }
+    }
+    class VolumeChangedEventClass
+    {
+        public void Message()
+        {
+            Console.WriteLine("Volume was changed");
+        }
+    }
+    class PlayerLockedEventClass
+    {
+        public void Message()
+        {
+            Console.WriteLine("Player was locked");
+        }
+    }
+    class PlayerUnlockedEventClass
+    {
+        public void Message()
+        {
+            Console.WriteLine("Player was unlocked");
+        }
+    }
+}
--- AudioPlayerProject/Program.cs
+++ AudioPlayerProject/Program.cs
@@ -10,12 +10,21 @@ namespace AudioPlayerProject
     {
         static void Main(string[] args)
         {
-            using (Player player = new Player())
-            {
-                player.LoadSongs("f://m//");
-                player.Play();
-                player.Dispose();
-            }
+            PlayerStartEventClass playerStartEvent = new PlayerStartEventClass();
+            PlayerStopEventClass playerStopEvent = new PlayerStopEventClass();
+            SongrPlayEventClass songPlayEvent = new SongrPlayEventClass();
+            SongListChangedEventClass songListChangedEvent = new SongListChangedEventClass();
+            VolumeChangedEventClass volumeChangedEvent = new VolumeChangedEventClass();
+            PlayerLockedEventClass playerLockedEvent = new PlayerLockedEventClass();
+            PlayerUnlockedEventClass playerUnlockedEvent = new PlayerUnlockedEventClass();
+            Player player = new Player();
+            player.PlayerStartEvent += playerStartEvent.Message;
+            player.PlayerStopEvent += playerStopEvent.Message;
+            player.SongPlayEvent += songPlayEvent.Message;
+            player.SongListChangedEvent += songListChangedEvent.Message;
+            player.VolumeChangedEvent += volumeChangedEvent.Message;
+            player.PlayerLockedEvent += playerLockedEvent.Message;
+            player.PlayerUnlockedEvent += playerUnlockedEvent.Message;
         }
     }
 }
